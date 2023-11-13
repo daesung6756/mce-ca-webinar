d04712987a5d3d8e32e2cd65f514842773cd5d07
daesung.lee <leeds019@naver.com>
2023-11-13 13:33:41 +0900

    default

--- js/customActivity.js
+++ js/customActivity.js
@@ -3,11 +3,9 @@ var connection = new Postmonger.Session();
 connection.trigger("ready")
 
 connection.on("initActivity", () => {
-    console.log("클릭했다.")
 })
 
 connection.on("clickedNext", () => {
-    console.log("완료했다.")
     var configuration;
     connection.trigger('updateActivity', configuration)
 })
